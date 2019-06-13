19e160b4d6164f0b41517f3eb0d19de91c97a25c
asktop <asktop@qq.com>
2019-06-13 09:49:29 +0800

    数值scan

--- ascan/scan_test.go
+++ ascan/scan_test.go
@@ -8,6 +8,8 @@ func TestConvertAssign(t *testing.T) {
 	soure := "abc"
 	var target string
 	err := ConvertAssign(&target, soure)
-	t.Log(err)
+	if err != nil {
+		t.Error(err)
+	}
 	t.Log(target)
 }
