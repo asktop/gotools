0fa7da9230af029188addb9da368d2dc6b0bdded
asktop <asktop@qq.com>
2019-10-18 21:54:57 +0800

    优化密码校验提示

--- avalid/isPwd.go
+++ avalid/isPwd.go
@@ -7,12 +7,15 @@ import (
 
 //检查密码
 // level: 密码强度级别
-// 	1：包含数字、字母
-// 	2：包含数字、字母、下划线
-// 	3：包含数字、字母、特殊字符
-// 	4：包含数字、大小写字母
-// 	5：包含数字、大小写字母、下划线
-// 	6：包含数字、大小写字母、特殊字符
+var pwdLevelTips = map[uint]string{
+	1: "必须包含数字、字母",
+	2: "必须包含数字、字母、下划线",
+	3: "必须包含数字、字母、特殊字符",
+	4: "必须包含数字、大小写字母",
+	5: "必须包含数字、大小写字母、下划线",
+	6: "必须包含数字、大小写字母、特殊字符",
+}
+
 type isPwd struct {
 	title    string
 	value    interface{}
@@ -26,10 +29,14 @@ func (c *isPwd) Check() (msg string, ok bool) {
 	if len(c.msgs) > 0 {
 		msg = c.msgs[0]
 	}
+	levelTip := "不符合要求"
+	if tip, ok := pwdLevelTips[c.level]; ok {
+		levelTip = tip
+	}
 	if len(c.length) == 0 {
 		if !astring.IsPwd(c.valueStr, c.level) {
 			if len(c.msgs) == 0 {
-				msg = fmt.Sprintf("%s不符合要求", c.title)
+				msg = fmt.Sprintf("%s%s", c.title, levelTip)
 			}
 			return msg, false
 		}
@@ -42,7 +49,7 @@ func (c *isPwd) Check() (msg string, ok bool) {
 				} else {
 					lenStr = fmt.Sprintf("%d 至 %d", c.length[0], c.length[1])
 				}
-				msg = fmt.Sprintf("%s不符合要求，且长度必须为 %s", c.title, lenStr)
+				msg = fmt.Sprintf("%s%s，且长度必须为 %s", c.title, levelTip, lenStr)
 			}
 			return msg, false
 		}
