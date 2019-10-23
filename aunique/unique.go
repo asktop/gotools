cf1adc2e63d5c50e121ba99761cb77b91c935f34
asktop <asktop@qq.com>
2019-10-23 14:12:57 +0800

    优化唯一序号

--- aunique/unique.go
+++ aunique/unique.go
@@ -2,64 +2,83 @@ package aunique
 
 import (
 	"fmt"
-	"github.com/asktop/gotools/amap"
 	"github.com/asktop/gotools/astring"
 	"github.com/asktop/gotools/atime"
 	"strconv"
 	"strings"
 	"sync"
+	"sync/atomic"
 )
 
-var prefixLast = amap.NewStrIfaceMap()
+var prefixLast = map[string]*last{}
 
 type last struct {
 	mu        sync.RWMutex
-	prefix    string //序号前缀
-	length    int    //数字序号长度
-	timestamp string //时间戳
-	number    int    //当前时间戳内序号
+	timestamp string //当前时间戳
+	number    int64  //当前时间戳内序号
 }
 
 // 唯一序号
-// @param length 数字序号长度，20到23位（不包括前缀，12位年月日时分秒+n位时间戳+4位同时间戳内序号+1位验证码）
+// @param length 序号数字部分长度，20到23位（不包括前缀，12位年月日时分秒+n位纳秒时间戳+4位同时间戳内序号+1位验证码）
 // @param prefix 序号前缀
 func UniqueNo(numLength int, prefix ...string) string {
-	numberLen := 4 //同时间戳内序号长度
-	if numLength < 20 || numLength > 23 {
-		panic("UniqueNo numLength must gte 20 and lte 23")
-	}
 	//序号前缀
 	var prefixStr string
 	if len(prefix) > 0 {
 		prefixStr = strings.TrimSpace(prefix[0])
 	}
 	key := prefixStr + strconv.Itoa(numLength)
+	//序号数字部分长度
+	if numLength < 20 || numLength > 23 {
+		panic("UniqueNo numLength must gte 20 and lte 23")
+	}
+	//同时间戳内序号长度
+	sortLength := 4
+
+	//生成当前时间戳 12位年月日时分秒+n位纳秒时间戳
+	nanosecond := fmt.Sprintf("%d", atime.Now().Nanosecond())
+	timestamp := atime.Now().Format("060102150405") + astring.Substr(nanosecond, 0, numLength-12-sortLength-1)
+	var sort int64 = 1
 
 	//上一个序号
-	lastNo := prefixLast.SetOrGet(key, &last{mu: sync.RWMutex{}, prefix: prefixStr, length: numLength}).(*last)
-	//当前序号
-	lastNo.mu.Lock()
-	defer lastNo.mu.Unlock()
-	//生成当前时间戳 12位年月日时分秒+n位时间戳
-	nanosecond := atime.Now().Format("060102150405") + fmt.Sprintf("%d", atime.Now().Nanosecond())
-	timestamp := astring.Substr(nanosecond, 0, lastNo.length-numberLen-1)
-	if lastNo.timestamp == timestamp {
-		lastNo.number++
-	} else {
-		lastNo.timestamp = timestamp
-		lastNo.number = 1
+	if _, ok := prefixLast[key]; !ok {
+		prefixLast[key] = &last{mu: sync.RWMutex{}}
 	}
+	lastNo := prefixLast[key]
+	//更新上一个序号为当前序号
+	lastNo.mu.Lock()
+	if lastNo.timestamp != timestamp {
+		lastNo.timestamp = timestamp
+		atomic.StoreInt64(&lastNo.number, 1)
+	} else {
+		atomic.AddInt64(&lastNo.number, 1)
+		sort = lastNo.number
+	}
+	lastNo.mu.Unlock()
+
 	//生成4位同时间戳内序号
-	number := astring.IntToStr(lastNo.number, numberLen)
-	if len(number) > numberLen {
+	sortStr := astring.IntToStr(int(sort), sortLength)
+	if len(sortStr) > sortLength {
 		panic("UniqueNo 同时间戳内序号长度超长")
 	}
-	uniqueNo := lastNo.prefix + timestamp + number
+	uniqueNo := prefixStr + timestamp + sortStr
 	//生成最后1位校验码
+	uniqueNo += getChechNo(uniqueNo)
+	return uniqueNo
+}
+
+// 校验唯一序号是否合法
+func CheckUniqueNo(uniqueNo string) bool {
+	source := astring.Substr(uniqueNo, 0, len(uniqueNo)-1)
+	checkNo := astring.Substr(uniqueNo, 0, -1)
+	return checkNo == getChechNo(source)
+}
+
+// 生成最后1位校验码
+func getChechNo(source string) string {
 	var sum int
-	for _, c := range uniqueNo {
+	for _, c := range source {
 		sum += int(c)
 	}
-	uniqueNo += strconv.Itoa(sum % 10)
-	return uniqueNo
+	return strconv.Itoa(sum % 10)
 }
--- aunique/unique_test.go
+++ aunique/unique_test.go
@@ -1,6 +1,7 @@
 package aunique
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -9,21 +10,35 @@ import (
 func TestUniqueNo(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		time.Sleep(time.Millisecond * 300)
-		t.Log(UniqueNo(16))
+		t.Log(UniqueNo(20))
 	}
 
 	for i := 0; i < 10; i++ {
 		time.Sleep(time.Millisecond * 300)
-		t.Log(UniqueNo(18, "USER_"))
+		t.Log(UniqueNo(20, "USER_"))
 	}
 }
 
 func TestUniqueNo2(t *testing.T) {
 	for i := 0; i < 1010; i++ {
-		if i > 995 {
-			t.Log(UniqueNo(16))
-		} else {
-			UniqueNo(16)
-		}
+		fmt.Println(UniqueNo(20))
+		//if i > 995 {
+		//	fmt.Println(UniqueNo(20))
+		//} else {
+		//	UniqueNo(20)
+		//}
+	}
+}
+
+func BenchmarkUniqueNo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Println(UniqueNo(20))
+		//for i := 0; i < 100; i++ {
+		//	if i > 995 {
+		//		fmt.Println(UniqueNo(20))
+		//	} else {
+		//		UniqueNo(20)
+		//	}
+		//}
 	}
 }
