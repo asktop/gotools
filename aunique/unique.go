1e3f929354f9eea835ff924018dcc242cac1e109
asktop <asktop@qq.com>
2019-10-23 11:43:24 +0800

    优化唯一序号

--- aunique/unique.go
+++ aunique/unique.go
@@ -2,7 +2,6 @@ package aunique
 
 import (
 	"fmt"
-	"github.com/asktop/gotools/acast"
 	"github.com/asktop/gotools/amap"
 	"github.com/asktop/gotools/astring"
 	"github.com/asktop/gotools/atime"
@@ -15,51 +14,48 @@ var prefixLast = amap.NewStrIfaceMap()
 
 type last struct {
 	mu        sync.RWMutex
-	prefix    string
-	length    int
+	prefix    string //序号前缀
+	length    int    //数字序号长度
 	timestamp string //时间戳
-	number    int    //当前秒内序号
+	number    int    //当前时间戳内序号
 }
 
 // 唯一序号
-// @param length 序号长度，不少于16（不包括前缀）
+// @param length 数字序号长度，20到23位（不包括前缀，12位年月日时分秒+n位时间戳+4位同时间戳内序号+1位验证码）
 // @param prefix 序号前缀
-func UniqueNo(length int, prefix ...string) string {
-	if length < 16 || length > 20 {
-		panic("UniqueNo length must gte 16 and lte 20")
+func UniqueNo(numLength int, prefix ...string) string {
+	numberLen := 4 //同时间戳内序号长度
+	if numLength < 20 || numLength > 23 {
+		panic("UniqueNo numLength must gte 20 and lte 23")
 	}
 	//序号前缀
 	var prefixStr string
 	if len(prefix) > 0 {
 		prefixStr = strings.TrimSpace(prefix[0])
 	}
-	key := prefixStr + strconv.Itoa(length)
+	key := prefixStr + strconv.Itoa(numLength)
 
 	//上一个序号
-	var lastNo *last
-	lastNo = prefixLast.SetOrGet(key, &last{mu: sync.RWMutex{}, prefix: prefixStr, length: length}).(*last)
+	lastNo := prefixLast.SetOrGet(key, &last{mu: sync.RWMutex{}, prefix: prefixStr, length: numLength}).(*last)
 	//当前序号
 	lastNo.mu.Lock()
 	defer lastNo.mu.Unlock()
+	//生成当前时间戳 12位年月日时分秒+n位时间戳
 	nanosecond := atime.Now().Format("060102150405") + fmt.Sprintf("%d", atime.Now().Nanosecond())
-	timestamp := astring.Substr(nanosecond, 0, lastNo.length-4)
+	timestamp := astring.Substr(nanosecond, 0, lastNo.length-numberLen-1)
 	if lastNo.timestamp == timestamp {
 		lastNo.number++
 	} else {
 		lastNo.timestamp = timestamp
 		lastNo.number = 1
 	}
-	number := astring.IntToStr(lastNo.number, 3)
-	if len(number) > 3 {
-		tag := acast.ToUint8(astring.SubstrByEnd(number, 0, -2))
-		if tag <= 36 {
-			tag += 55 //A:64
-		} else {
-			tag += 87 //a:97
-		}
-		number = string(tag) + astring.Substr(number, 0, -2)
+	//生成4位同时间戳内序号
+	number := astring.IntToStr(lastNo.number, numberLen)
+	if len(number) > numberLen {
+		panic("UniqueNo 同时间戳内序号长度超长")
 	}
 	uniqueNo := lastNo.prefix + timestamp + number
+	//生成最后1位校验码
 	var sum int
 	for _, c := range uniqueNo {
 		sum += int(c)
