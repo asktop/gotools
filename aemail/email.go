af8a307fffa8f291481655556c32986c12df324c
asktop <asktop@qq.com>
2020-07-13 17:39:49 +0800

    优化：email

--- aemail/email.go
+++ aemail/email.go
@@ -9,7 +9,7 @@ type Config struct {
     Port     int    `json:"port"`     //SMTP服务器端口
     Email    string `json:"email"`    //发送用户用户名或邮箱
     Password string `json:"password"` //发送用户密码
-    Name     string `json:"name"`     //发送用户显示名
+    Username string `json:"username"` //发送用户显示名
 }
 
 type EmailClient struct {
@@ -17,7 +17,7 @@ type EmailClient struct {
     Port     int    //SMTP服务器端口
     Email    string //发送用户用户名或邮箱
     Password string //发送用户密码
-    Name     string //发送用户显示名
+    Username string //发送用户显示名
 }
 
 func NewEmailClient(config Config) *EmailClient {
@@ -26,24 +26,24 @@ func NewEmailClient(config Config) *EmailClient {
         Port:     config.Port,
         Email:    config.Email,
         Password: config.Password,
-        Name:     config.Name,
+        Username: config.Username,
     }
 }
 
-func New163EmailClient(config Config) *EmailClient {
+func New163EmailClient(email string, password string, username string) *EmailClient {
     return &EmailClient{
         Host:     "smtp.163.com",
         Port:     465,
-        Email:    config.Email,
-        Password: config.Password,
-        Name:     config.Name,
+        Email:    email,
+        Password: password,
+        Username: username,
     }
 }
 
 //发送邮件
 func (e *EmailClient) Send(subject string, body string, to ...string) error {
     m := gomail.NewMessage()
-    m.SetHeader("From", e.Name+"<"+e.Email+">")
+    m.SetHeader("From", e.Username+"<"+e.Email+">")
     m.SetHeader("To", to...)
     m.SetHeader("Subject", subject) //设置邮件主题
     m.SetBody("text/html", body)    //设置邮件正文
--- aemail/email_test.go
+++ aemail/email_test.go
@@ -8,7 +8,7 @@ import (
 func TestSendEmail(t *testing.T) {
     url := "http://www.baidu.com"
     body := `<a href="` + url + `"> 点我继续 </a><br><br>若以上链接无法点击，请复制下面内容到浏览器地址栏并访问<br>` + url
-    err := New163EmailClient(Config{Email: "asktop2010@163.com", Password: "", Name: "asktop"}).Send("测试123", body, "asktop2010@163.com")
+    err := New163EmailClient("asktop2010@163.com", "", "asktop").Send("测试123", body, "asktop2010@163.com")
     if err != nil {
         fmt.Println(err)
     } else {
