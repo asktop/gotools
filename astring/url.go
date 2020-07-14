66ef8fe46692b6d0a3b6dfcd09f12cd2cc50e226
asktop <asktop@qq.com>
2020-07-14 16:35:36 +0800

    优化：astring

--- astring/url.go
+++ astring/url.go
@@ -3,15 +3,20 @@ package astring
 import "strings"
 
 //拼接URL
-func JoinURL(url ...string) string {
-	var rs []string
-	for _, u := range url {
-		u = strings.TrimSpace(u)
-		u = strings.TrimPrefix(u, "/")
-		u = strings.TrimPrefix(u, `\`)
-		u = strings.TrimSuffix(u, "/")
-		u = strings.TrimSuffix(u, `\`)
-		rs = append(rs, u)
-	}
-	return strings.Join(rs, `/`)
+func JoinURL(uris ...string) string {
+    var url string
+    var urisTemp []string
+    for _, uri := range uris {
+        uri = strings.Trim(uri, " ")
+        uri = strings.Trim(uri, "/")
+        uri = strings.Trim(uri, `\`)
+        if uri != "" {
+            urisTemp = append(urisTemp, uri)
+        }
+    }
+    url = strings.Join(urisTemp, "/")
+    if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+        url = "/" + url
+    }
+    return url
 }
