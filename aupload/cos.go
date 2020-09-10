3eb663a8dc72a9493556a0a26cd6780964f2c495
asktop <asktop@qq.com>
2020-09-10 23:57:41 +0800

    新增：获取文件列表

--- aupload/cos.go
+++ aupload/cos.go
@@ -238,7 +238,7 @@ func (c *CosClient) CopyFile(source_url_filePathName string, filePathName string
 
 //获取文件列表
 func (c *CosClient) GetFiles(fileDir string) (fileInfos []FileInfo, err error) {
-    dir := filepath.Join(c.GetBucket(), fileDir)
+    dir := strings.TrimPrefix(astring.JoinURL(c.GetBucket(), fileDir), "/")
     opt := &cos.BucketGetOptions{
         Prefix: dir,
     }
